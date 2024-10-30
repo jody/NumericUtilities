388680e99a061e354dff1defb0f33175c5f88d38
jody <jody@acm.org>
2024-10-29 22:41:45 -0600

    Updated project with Utilities content and a JUnit test class

--- Utilities.java
+++ Utilities.java
@@ -1,3 +1,69 @@
 /* Utilities.java */
+/**
+ * Template for class that implements numeric utility functions.
+ * Initial functions address numeric sequences.
+ *
+ * @author Dr. Jody Paul
+ * @version 1.1.4
+ */
 public final class Utilities {
+    /**
+     * Hide constructor because this is a utility class.
+     */
+    Utilities() { }
+
+    /**
+     * Determines the number of unique connections between
+     * fully-connected members of a group of the specified
+     * size.
+     *
+     * @param  numPeople the number of people in the group
+     *                   (valid for numPeople >= 0)
+     * @return the number of connections
+     */
+    public static long numberOfConnections(int numPeople) {
+        if (numPeople == 0) return 0;
+        else return numberOfConnections(numPeople - 1) + (numPeople - 1);
+    }
+
+    /**
+     * Determines the number in the Fibonacci sequence
+     * corresponding to the index given as the parameter.
+     * The sequence starts with fibonacci(0)==0 and
+     * fibonacci(1)==1.
+     *
+     * @param  index the index of the desired value
+     *                   (valid for index >= 0)
+     * @return the Fibonacci number at the specific index
+     */
+    public static long fibonacci(int index) {
+        return index;
+    }
+
+    /**
+     * Determine the number in a Tribonacci sequence
+     * corresponding to the index given as the parameter.
+     * The sequence is defined as follows:
+     * tribonacci(0)==0;
+     * tribonacci(1)==0;
+     * tribonacci(2)==1;
+     * tribonacci(n)==tribonacci(n-2)+tribonacci(n-2)+tribonacci(n-3)
+     */
+    public static long tribonacci(int index) {
+        return index;
+    }
+
+    /**
+     * Determine the number in a Fibonacci primes sequence
+     * corresponding to the index given as the parameter.
+     * A Fibonacci prime number is a Fibonacci number that is also prime.
+     * The first few Fibonacci primes are the following.
+     * fibonacciPrimes(0)==2;
+     * fibonacciPrimes(1)==3;
+     * fibonacciPrimes(2)==5;
+     * fibonacciPrimes(2)==13;
+     */
+    public static long fibonacciPrimes(int index) {
+        return index;
+    }
 }
---
+++ UtilitiesTest.java
@@ -0,0 +1,45 @@
+/* UtilitiesTest.java */
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+/**
+ * Tests for the Utilities class.
+ *
+ * @author  Dr. Jody Paul
+ * @version 1.1.0
+ */
+public class UtilitiesTest {
+    /** No-parameter constructor for test class UtilitiesTest. */
+    public UtilitiesTest() {
+    }
+
+    /** Set up the test fixture before every test case method is invoked. */
+    @BeforeEach
+    public void setUp() {
+    }
+
+    /** Tear down the test fixture after every test case method exits. */
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /** Test numberOfConnections with single-digit positive values. */
+    @Test
+    public void numberOfConnectionsTestSmallValidParams() {
+        assertEquals(0, Utilities.numberOfConnections(0));
+        assertEquals(0, Utilities.numberOfConnections(1));
+        assertEquals(1, Utilities.numberOfConnections(2));
+        assertEquals(3, Utilities.numberOfConnections(3));
+        assertEquals(6, Utilities.numberOfConnections(4));
+        assertEquals(10, Utilities.numberOfConnections(5));
+    }
+
+    /** Test numberOfConnections with a range of positive values. */
+    @Test
+    public void numberOfConnectionsTestLargerValidParams() {
+        assertEquals(4950L, Utilities.numberOfConnections(100));
+        assertEquals(499500L, Utilities.numberOfConnections(1000));
+        assertEquals(49995000L, Utilities.numberOfConnections(10000));
+    }
+}
